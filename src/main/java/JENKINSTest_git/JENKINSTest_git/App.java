0fe64978f2e1af1fc27b4ea4f0e4f7b93c2b7c1f
varshabad <varshapawar22@gmail.com>
2021-06-04 14:00:31 +0530

    Test first commint

---
+++ src/main/java/JENKINSTest_git/JENKINSTest_git/App.java
@@ -0,0 +1,13 @@
+package JENKINSTest_git.JENKINSTest_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
